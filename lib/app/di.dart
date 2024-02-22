2abd8b25134f11ccbc13f249dbfcf2a995f7b1f6
osamasabry9 <osamasabry324@gmail.com>
2024-02-23 00:59:43 +0200

    added SignUp instance

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:doctor/core/networking/api_service.dart';
 import 'package:doctor/features/login/presentation/cubit/login_cubit.dart';
+import 'package:doctor/features/sign_up/data/repositories/sign_up_reppo.dart';
+import 'package:doctor/features/sign_up/presentation/cubit/sign_up_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 import '../core/networking/dio_factory.dart';
@@ -8,13 +10,20 @@ import '../features/login/data/repositories/login_repo.dart';
 
 final getIt = GetIt.instance;
 
-Future<void> setupGetIt()async {
-
+Future<void> setupGetIt() async {
   // dio & api service
   Dio dio = await DioFactory.getDio();
-getIt.registerLazySingleton<ApiService>(() =>   ApiService(dio),);
+  getIt.registerLazySingleton<ApiService>(
+    () => ApiService(dio),
+  );
 
 // login instance
-  getIt.registerLazySingleton<LoginRepo>(() =>  LoginRepo(getIt<ApiService>()));
-  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt<LoginRepo>()));
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt<ApiService>()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt<LoginRepo>()));
+
+  // SignUp instance
+  getIt
+      .registerLazySingleton<SignUpRepo>(() => SignUpRepo(getIt<ApiService>()));
+
+  getIt.registerFactory<SignUpCubit>(() => SignUpCubit(getIt<SignUpRepo>()));
 }

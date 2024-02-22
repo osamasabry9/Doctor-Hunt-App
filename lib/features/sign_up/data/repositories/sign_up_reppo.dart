13791ec82e2e2c58b117206a6e65642ce3b52b47
osamasabry9 <osamasabry324@gmail.com>
2024-02-23 01:01:52 +0200

    Create Sign Up Repo

---
+++ lib/features/sign_up/data/repositories/sign_up_reppo.dart
@@ -0,0 +1,21 @@
+import '../../../../core/errors/api_error_handler.dart';
+import '../../../../core/networking/api_result.dart';
+import '../../../../core/networking/api_service.dart';
+import '../models/sign_up_request_body.dart';
+import '../models/sign_up_response.dart';
+
+class SignUpRepo {
+  final ApiService _apiService;
+
+  SignUpRepo(this._apiService);
+
+  Future<ApiResult<SignUpResponse>> signUp(
+      SignUpRequestBody signUpRequestBody) async {
+    try {
+      final response = await _apiService.signUp(signUpRequestBody);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}

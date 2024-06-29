68964ead9bcedb74094de30231ffe767c8691241
osamasabry9 <osamasabry324@gmail.com>
2024-06-29 17:06:15 +0300

    Refactor theme data and initial route logic in DocHuntApp
    Move theme data configuration to include primary color and scaffold
    background color. Update initial route based on user login status.
    Add checkIfLoggedInUser function for determining user login status.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -3,11 +3,26 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app/di.dart';
 import 'app/doc_hunt_app.dart';
+import 'core/local_storage/app_constants_keys.dart';
+import 'core/local_storage/secure_storage_helper.dart';
+import 'core/helper/extensions.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
   // To fix texts being hidden bug in flutter_screenutil in release mode.
   await ScreenUtil.ensureScreenSize();
+  await checkIfLoggedInUser();
   runApp(const DocHuntApp());
+}
+
+/// To check if user is logged in or not.
+checkIfLoggedInUser() async {
+  String? userToken =
+      await SecureStorageHelper.getSecuredString(AppConstantsKeys.userToken);
+  if (!userToken.isNullOrEmpty()) {
+    isLoggedInUser = true;
+  } else {
+    isLoggedInUser = false;
+  }
 }
